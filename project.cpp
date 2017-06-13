698f2dd37d01a24ef079acb0bb601ca1c70f8eb4
Todor <todor@debian>
2017-06-14 00:25:08 +0300

    Reading graph from file

---
+++ project.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <fstream>
+#include <string>
+
+void readGraph(std::string file_name, std::vector<std::vector<int>>& graph);
+void printGraph(const std::vector<std::vector<int>>& graph);
+void singleDFS(std::vector<std::vector<int>>& graph, int startNode);
+
+int main() {
+	std::vector<std::vector<int>> graph;
+	readGraph("graph.in", graph);
+	printGraph(graph);
+	return 0;
+}
+
+void readGraph(std::string file_name, std::vector<std::vector<int>>& graph) {
+	std::fstream file(file_name, std::ios::in);
+	
+	int numberNodes;
+	if(file.is_open()) {
+		file >> numberNodes;
+		graph = std::vector<std::vector<int>>(numberNodes, std::vector<int>(numberNodes));
+		for(int i = 0; i < numberNodes; i++) {
+			for(int j = 0; j < numberNodes; j++) {
+				file >> graph[i][j];
+			}
+		}
+
+		file.close();
+	}
+}
+
+void printGraph(const std::vector<std::vector<int>>& graph) {
+	if(graph.empty()) {
+		return;
+	}
+
+	for(auto iter = graph.begin(); iter != graph.end(); iter++) {
+		for(auto node_iter = iter -> begin(); node_iter != iter -> end(); node_iter++) {
+			std::cout << *node_iter << ' ';
+		}
+		std::cout << std::endl;
+	}
+}
+
+
